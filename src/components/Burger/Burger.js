9cc4caa92952deb6b97da5cd904bfee885543c01
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-12-06 16:12:06 -0800

    added Build Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
-import classes from './Burger.module.css';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import classes from "./Burger.module.css";
 
-const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-        .map(ingredient => {
-            return [...Array(props.ingredients[ingredient])].map((_, i) => {
-                return <BurgerIngredient key={ingredient + i} type={ingredient} />
-            })
-        });
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
-}
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredient => {
+      return [...Array(props.ingredients[ingredient])].map((_, i) => {
+        return <BurgerIngredient key={ingredient + i} type={ingredient} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  // This log (without .reduce attached) shows us that the array isn't actually empty so we can't use the length as an indicator; we add .reduce() to the end of the array above to flatten it into 1 array
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,22 +1,23 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 
-import Burger from '../../components/Burger/Burger'
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 export default class BurgerBuilder extends Component {
-    state = {
-        ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
-        }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
-    render() {
-        return (
-            <Fragment>
-                <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
-            </Fragment>
-        )
-    }
+  };
+  render() {
+    return (
+      <Fragment>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+      </Fragment>
+    );
+  }
 }
