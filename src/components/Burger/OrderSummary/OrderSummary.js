a5e8d32a769b6361b2ea99e6de5d131667f2c53c
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-12-10 14:59:30 -0800

    initial project wrap up/folder cleanup

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  // map through ingredients and create dynamic list with totals
-  const ingredientSummary = Object.keys(props.ingredients).map(ingredient => {
-    return (
-      <li key={ingredient}>
-        <span style={{ textTransform: "capitalize" }}>{ingredient}</span>:{" "}
-        {props.ingredients[ingredient]}
-      </li>
+class OrderSummary extends Component {
+  // Class based component for debugging with componentDidUpdate()
+  componentDidUpdate() {
+    console.log("[OrderSummary] did update");
+  }
+
+  render() {
+    // map through ingredients and create dynamic list with totals
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      ingredient => {
+        return (
+          <li key={ingredient}>
+            <span style={{ textTransform: "capitalize" }}>{ingredient}</span>:{" "}
+            {this.props.ingredients[ingredient]}
+          </li>
+        );
+      }
     );
-  });
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
